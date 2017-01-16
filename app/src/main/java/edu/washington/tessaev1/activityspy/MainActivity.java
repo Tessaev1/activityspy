b746a1ae0213f95fbe5000d97a11bdc258bd9885
Tessaev1 <tessaev1@uw.edu>
2017-01-16 15:25:30 -0800

    added all events on the activity lifecycle

--- app/src/main/java/edu/washington/tessaev1/activityspy/MainActivity.java
+++ app/src/main/java/edu/washington/tessaev1/activityspy/MainActivity.java
@@ -12,12 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate event fired");
     }
 
     @Override
     protected void onStart(){
         super.onStart();
         Log.i(TAG, "onStart event fired");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(TAG, "onResume event fired");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(TAG, "onPause event fired");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(TAG, "onStop event fired");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.i(TAG, "onRestart event fired");
     }
 
     @Override
