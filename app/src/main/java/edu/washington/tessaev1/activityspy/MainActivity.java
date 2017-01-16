6d8be9d64e3c1247c66035a9a49758d6fd1b57e9
Tessaev1 <tessaev1@uw.edu>
2017-01-16 10:35:44 -0800

    initial code setup

---
+++ app/src/main/java/edu/washington/tessaev1/activityspy/MainActivity.java
@@ -0,0 +1,28 @@
+package edu.washington.tessaev1.activityspy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.I(TAG, "onStart event fired");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.E(TAG, "We're going down, Captain!");
+    }
+}
